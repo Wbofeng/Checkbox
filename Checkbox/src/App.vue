3cda117151f104a40995b8dc86fef7490f5c7613
Wbofeng <1464226975@qq.com>
2018-05-02 21:42:34 +0800

    add main component

--- Checkbox/src/App.vue
+++ Checkbox/src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div id="app">
+    <checkbox></checkbox>
   </div>
 </template>
 
 <script>
+import checkbox from './components/Main'
 export default {
   name: 'App',
+  components: {
+    checkbox
+  }
 }
 </script>
 
@@ -16,6 +21,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ Checkbox/src/components/Main.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="checkbox">
+    <div class="checkbox-header"></div>
+    <div class="checkbox-main">
+      <div v-for="(data, index) in datas" :key="index" class="checkbox-main-div"></div>
+    </div>
+  </div>
+</template>
+<style scoped>
+.checkbox {
+  margin: 20px;
+  width: 20vw;
+  height: 30vw;
+  background: whitesmoke;
+}
+
+.checkbox:hover {
+  box-shadow: 0 2px 12px 0 rgba(0,0,0,.1);
+}
+
+.checkbox-header {
+  width: 100%;
+  height: 10%;
+  border-bottom: 1px solid white;
+}
+</style>
+<script>
+export default {
+  data () {
+    return {
+      data: [{}, {}, {}]
+    }
+  }
+}
+</script>
